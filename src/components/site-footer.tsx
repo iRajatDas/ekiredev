3f1b4157c63aaaf4efa39426f07004bda1073dc2
Rajat Das <draj123258@gmail.com>
2025-01-10 21:46:04 +0530

    ui: footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -3,9 +3,9 @@ import React from "react";
 const SiteFooter = () => {
   return (
     <footer className="bg-white border-t px-4 sm:px-6 md:px-8 py-4 text-sm text-gray-500">
-      <div className="flex justify-between">
+      <div className="flex justify-between max-md:flex-col-reverse max-md:gap-5">
         <span>Copyright © 2025 Ekire LTD. All rights reserved.</span>
-        <div className="flex gap-4">
+        <div className="flex gap-4 max-md:flex-col">
           <a href="#" className="hover:underline">
             Privacy & Policy
           </a>
